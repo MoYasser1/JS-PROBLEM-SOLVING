c5ef75020f74e30c67c4a159375630c6ea36945c
MoYasser1 <mohamed0yasser@gmail.com>
2024-01-13 20:56:13 +0200

    solving num1

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-let numbers= [10,10,20,20,30,30,50,50]
+let numbers = [10, 10, 20, 20, 30, 30, 50, 50];
+let uniqueNumbers = [...new Set(numbers)];
 
-
-for (let index = 0; index.valueOf < array.length; index++) {
-    const element = array[index.valueOf];
-    
-}
-
-function getduplcat(data).fillter(valueOf,)
-
-
-console.log(numbers);
+console.log(uniqueNumbers);
