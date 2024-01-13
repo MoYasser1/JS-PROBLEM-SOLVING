b0984f25f837857ae1b753dd387bdd775e44bf96
MoYasser1 <mohamed0yasser@gmail.com>
2024-01-13 21:02:04 +0200

    example

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ let numbers = [10, 10, 20, 20, 30, 30, 50, 50];
 let uniqueNumbers = [...new Set(numbers)];
 
 console.log(uniqueNumbers);
+
+
+
+
+
+
+// In the provided code, there are several concepts being used in the JavaScript language to work with arrays of numbers.
+
+// First, we have the array `numbers` that contains a set of numbers. In this example, the numbers are `[10, 10, 20, 20, 30, 30, 50, 50]`. It appears that the numbers are repeated in the array.
+
+// Then, a new concept in JavaScript called a "Set" is utilized to remove duplicate elements from the array. Here, we use the expression `new Set(numbers)` to create a new set that contains only the unique elements from the array. In this case, the set will contain the numbers `[10, 20, 30, 50]`.
+
+// Finally, the spread syntax `...` is used to convert the set into a new array. The unique elements are stored in the `uniqueNumbers` array.
+
+// At the end of the code, `console.log(uniqueNumbers)` is used to print the `uniqueNumbers` array to the console in the browser or runtime environment. The unique numbers `[10, 20, 30, 50]` will be displayed in the console.
