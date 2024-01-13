546d7229561707400b903da47e9e964c06afce88
MoYasser1 <mohamed0yasser@gmail.com>
2024-01-13 20:46:27 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let numbers= [10,10,20,20,30,30,50,50]
+
+
+for (let index = 0; index.valueOf < array.length; index++) {
+    const element = array[index.valueOf];
+    
+}
+
+function getduplcat(data).fillter(valueOf,)
+
+
+console.log(numbers);
