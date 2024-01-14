6dfe04ca62741a2d6e0a73509109edd511023367
MoYasser1 <mohamed0yasser@gmail.com>
2024-01-14 23:53:07 +0200

    edabit problem num 2

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-let numbers = [10, 10, 20, 20, 30, 30, 50, 50];
-let uniqueNumbers = [...new Set(numbers)];
+// **let numbers = [10, 10, 20, 20, 30, 30, 50, 50];
+// **let uniqueNumbers = [...new Set(numbers)];
 
-console.log(uniqueNumbers);
-
-
-
-
+// **console.log(uniqueNumbers);
 
 
 // In the provided code, there are several concepts being used in the JavaScript language to work with arrays of numbers.
@@ -17,3 +13,21 @@ console.log(uniqueNumbers);
 // Finally, the spread syntax `...` is used to convert the set into a new array. The unique elements are stored in the `uniqueNumbers` array.
 
 // At the end of the code, `console.log(uniqueNumbers)` is used to print the `uniqueNumbers` array to the console in the browser or runtime environment. The unique numbers `[10, 20, 30, 50]` will be displayed in the console.
+//  **************************edabit*************************
+
+//*1*This is an introduction to how challenges on Edabit work. In the Code tab above you'll see a starter function that looks like this:
+
+// function hello() {
+
+// }
+// All you have to do is type return "hello edabit.com" between the curly braces { } and then click the Check button. If you did this correctly, the button will turn red and say SUBMIT FINAL. Click it and see what happens.
+
+// Notes
+// The returned string must be in all lowercase letters.
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.
+
+// **Solutions**
+// function hello() {
+//     return "hello edabit.com";
+// }
