94af5ca571ade5cb4abf6a78d21f8a8dd4c73090
MoYasser1 <mohamed0yasser@gmail.com>
2024-01-14 23:57:04 +0200

    Area of a Triangle problem num 2

--- script.js
+++ script.js
@@ -31,3 +31,28 @@
 // function hello() {
 //     return "hello edabit.com";
 // }
+
+// **2-Area of a Triangle
+// Write a function that takes the base and height of a triangle and return its area.
+
+// Examples
+// triArea(3, 2) ➞ 3
+
+// triArea(7, 4) ➞ 14
+
+// triArea(10, 10) ➞ 50
+// Notes
+// The area of a triangle is: (base * height) / 2
+// Don't forget to return the result.
+// If you get stuck on a challenge, find help in the Resources tab.
+// If you're really stuck, unlock solutions in the Solutions tab.
+
+// **Solutions**
+// function triArea(base, height) {
+//     return (base * height) / 2;
+// }
+
+// // Examples
+// console.log(triArea(3, 2)); // Output: 3
+// console.log(triArea(7, 4)); // Output: 14
+// console.log(triArea(10, 10)); // Output: 50
